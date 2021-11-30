e3c3b4f9579b2429c2e8f2ac10e3ca864e32b085
Allyson Brito <allyson.brito@techlead.com.br>
2021-11-29 22:53:13 -0300

    ajuste do teste de exceção

--- dojo2/src/dojo2/LojaTest.java
+++ dojo2/src/dojo2/LojaTest.java
@@ -1,14 +1,69 @@
 package dojo2;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 
 class LojaTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+    public void TesteRemoverProduto() {
+    
+	Loja loja = new Loja();
+    Produto prd = new Produto("Arroz",20,10);
+    
+    
+    loja.addProduto(prd);
+    loja.RemoveProduto("Arroz");
+    
+    assertEquals(false,loja.Busca("Arroz"));
+    
 	}
+	
+    @Test
+    public void TesteListarProdutos() {
+    
+	Loja loja = new Loja();
+    Produto prd1 = new Produto("Arroz",20,10);
+    Produto prd2 = new Produto("Feijão",5,15);
+    Produto prd3 = new Produto("Macarrão",4,10);
+    
+    loja.addProduto(prd1);
+    loja.addProduto(prd2);
+    loja.addProduto(prd3);
+    
+    
+    assertEquals(3,loja.Quantidade());
+
+    }
+
+	
+	@Test
+    public void TesteBuscarProduto() {
+    
+	Loja loja = new Loja();
+    Produto prd = new Produto("Arroz",20,10);
+    
+    loja.addProduto(prd);
+    
+    assertEquals(true,loja.Busca("Arroz"));
+
+	}
+
+	@Test //(expected = IllegalArgumentException.class)
+	public void TesteExecaoProduto() {
+		
+		Loja loja = new Loja();
+		Produto prd = new Produto("Arroz",0,10);
+		//loja.addProduto(prd);
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			loja.addProduto(prd);
+			
+		});
+		
+	}
+
 
 }
--- dojo2/src/dojo2/ProdutoTest.java
+++ dojo2/src/dojo2/ProdutoTest.java
@@ -9,7 +9,6 @@ class ProdutoTest {
 	@Test
     public void TesteAddProduto() {
     
-	
     Produto prd = new Produto("Arroz",20,10);
    
     assertEquals("Arroz",prd.getName());
@@ -18,64 +17,6 @@ class ProdutoTest {
 
     }
 	
-	@Test
-    public void TesteRemoverProduto() {
-    
-	Loja loja = new Loja();
-    Produto prd = new Produto("Arroz",20,10);
-    
-    loja.addProduto(prd);
-    loja.RemoveProduto("Arroz");
-    
-    assertEquals(false,loja.Busca("Arroz"));
-    
-	}
 	
-    @Test
-    public void TesteListarProdutos() {
-    
-	Loja loja = new Loja();
-    Produto prd1 = new Produto("Arroz",20,10);
-    Produto prd2 = new Produto("Feijão",5,15);
-    Produto prd3 = new Produto("Macarrão",4,10);
-    
-    loja.addProduto(prd1);
-    loja.addProduto(prd2);
-    loja.addProduto(prd3);
-    
-    
-    assertEquals(3,loja.Quantidade());
-
-    }
-
-	
-	@Test
-    public void TesteBuscarProduto() {
-    
-	Loja loja = new Loja();
-    Produto prd = new Produto("Arroz",20,10);
-    
-    loja.addProduto(prd);
-    
-    assertEquals(true,loja.Busca("Arroz"));
-//    assert.AssertTrue(loja.Busca("Arroz"));
-//    
-//    assert.assertTrue(loja.Busca("Arroz"));
-   // assert.
-	}
-
-	
-//	@Test(expected = IllegalArgumentException.class)
-//	public void TesteExcecaoProduto() {
-//	
-//		Loja loja = new Loja();
-//	    Produto prd1 = new Produto("Arroz",0,10);
-//	    
-//	    loja.addProduto(prd1);
-//	    
-//		
-//	}
-
-
 
 }
